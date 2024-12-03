c2b0a3cd0431901634e86eafbacc43287cbd51be
BlairBelle3 <juanenriquealvarezlopez4@gmail.com>
2024-12-02 18:33:04 -0600

    Add files via upload

--- listarTarjeta.js
+++ listarTarjeta.js
@@ -1,15 +1,14 @@
 
-//recuperando el cuerpo de la tabla
-var cuerpo_tarjeta_categoria = document.querySelector('#contenedor_tarjeta_categoria');
-var resultado_tarjetas_categorias = "";
+//recuperando el cuerpo donde iran almacenadas las marcas
+var cuerpo_marca = document.getElementById('contenedor_cartas_marcas');
+var resul_marca = "";
 
-//listar tarjetas de las categorias en el inicio
-ListarTarjetasCategoria();
-function ListarTarjetasCategoria () {
-    $.post("php/categoria/listarTarjetas.php",
-        function (data, textStatus) {
-            resultado_tarjetas_categorias = data;
-            cuerpo_tarjeta_categoria.innerHTML = resultado_tarjetas_categorias;
+ListarMarca();
+function ListarMarca(){
+    $.post("php/marca/listarTarjetas.php",
+        function (data, textStatux) {
+            resul_marca = data;
+            cuerpo_marca.innerHTML = resul_marca;
         }
     );
 }
